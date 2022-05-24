e9bbe16b3178c9f7d8ff53cbdf6247fe90f0b97e
Hiromu <94920930+hiromuyamada@users.noreply.github.com>
2022-05-25 01:38:45 +0900

    create mock

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(["aaaa", "iiii"]);
+
+  const [completeTodos, setCompleteTodos] = useState(["uuuuu"]);
+
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
